1ca38f84d1c2cc7edb51c4be66cc0f3a0f3cb04e
caitlin <caitlin.welsh9@gmail.com>
2018-02-19 13:15:53 -0700

    updated next handler

--- src/components/Album.js
+++ src/components/Album.js
@@ -51,6 +51,14 @@ class Album extends Component {
     this.setSong(newSong);
     this.play(newSong);
   }
+
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
   }
 
 
@@ -94,7 +102,8 @@ class Album extends Component {
           isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-          handlePrevClick={() => this.handlePrevClick()} 
+          handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
